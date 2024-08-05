dcaf1d830874da9f40aac8d9e8d05594476c67c5
daviavila2007 <139869720+daviavila2007@users.noreply.github.com>
2024-08-05 09:44:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function home(){
+    window.location.href = "home.html"
+}
+function carta(){
+    window.location.href = "carta.html"
+}
+function voltar(){
+    window.location.href = "home.html"
+}
