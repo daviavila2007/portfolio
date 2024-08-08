99b2183d4d6ed6fc4a92f560395d6b9ea4415c8b
daviavila2007 <139869720+daviavila2007@users.noreply.github.com>
2024-08-08 12:01:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,69 @@ function carta(){
 function voltar(){
     window.location.href = "home.html"
 }
+function areas(){
+    window.location.href = "trimestres.html"
+}
+function trimestre1(){
+    window.location.href = "trimestre1.html"
+}
+function trimestre2(){
+    window.location.href = "trimestre2.html"
+}
+function trimestre3(){
+    window.location.href = "trimestre3.html"
+}
+function voltar2(){
+    window.location.href = "trimestres.html"
+}
+function mtm1(){
+    window.location.href = ("mtm1.html")
+}
+function voltar3(){
+    window.location.href = ("trimestre1.html")
+}
+function natureza1(){
+    window.location.href = ("natureza1.html")
+}
+function tecnico1(){
+    window.location.href = ("tecnico1.html")
+}
+function humanas1(){
+    window.location.href = ("humanas1.html")
+}
+function linguagens1(){
+    window.location.href = ("linguagens1.html")
+}
+function mtm2(){
+    window.location.href = ("mtm2.html")
+}
+function natureza2(){
+    window.location.href = ("natureza2.html")
+}
+function tecnico2(){
+    window.location.href = ("tecnico2.html")
+}
+function humanas2(){
+    window.location.href = ("humanas2.html")
+}
+function linguagens2(){
+    window.location.href = ("linguagens2.html")
+}
+function mtm3(){
+    window.location.href = ("mtm3.html")
+}
+function natureza3(){
+    window.location.href = ("natureza3.html")
+}
+function tecnico3(){
+    window.location.href = ("tecnico3.html")
+}
+function humanas3(){
+    window.location.href = ("humanas3.html")
+}
+function linguagens3(){
+    window.location.href = ("linguagens3.html")
+}
+function calchoras(){
+    window.location.href = ("calchoras.html")
+}
