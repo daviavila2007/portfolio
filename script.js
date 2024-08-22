d9f5b812cdf682f7af993621a81e3e72fe0ba741
daviavila2007 <139869720+daviavila2007@users.noreply.github.com>
2024-08-21 21:24:35 -0300

    aa

--- script.js
+++ script.js
@@ -83,5 +83,5 @@ function ante(){
     window.location.href = "https://www.canva.com/design/DAFxdUgkBQk/Nx9E6RXl0q2y3P0VKuHBew/edit?utm_content=DAFxdUgkBQk&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
 }
 function ppe(){
-    window.location.href = "ppe.html"
+    window.location.href = ("ppe.html")
 }
